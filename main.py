87917f363a081be5f59249b9f480302e7321e28a
Hanns Adrian Böhme <hannsadrian@gmail.com>
2023-11-22 22:39:41 +0100

    Update orientation logic

--- main.py
+++ main.py
@@ -5,9 +5,10 @@ from pybricks.ev3devices import (Motor, TouchSensor, ColorSensor,
 from pybricks.parameters import Port, Stop, Direction, Button, Color
 from pybricks.tools import wait, StopWatch, DataLog
 from pybricks.robotics import DriveBase
-from pybricks.media.ev3dev import SoundFile, ImageFile
+from pybricks.media.ev3dev import SoundFile, ImageFile, Image
 
 import orientation
+import turning
 
 ORIENTING_STATE = "ORIENTING_STATE"
 CLIMBING_STATE = "CLIMBING_STATE"
@@ -20,23 +21,29 @@ CLIMBING_STATE = "CLIMBING_STATE"
 ev3 = EV3Brick()
 program_state = ORIENTING_STATE
 
-kill_switch = TouchSensor(Port.S4)
+#kill_switch = TouchSensor(Port.S4)
 distance_sensor = UltrasonicSensor(Port.S1)
 vertical_gyro = GyroSensor(Port.S2)
 vertical_gyro.reset_angle(0)
 horizontal_gyro = GyroSensor(Port.S3)
 horizontal_gyro.reset_angle(0)
 
-left_drive_motor = Motor(Port.A)
-right_drive_motor = Motor(Port.D)
+left_drive_motor = Motor(Port.D)
+right_drive_motor = Motor(Port.A)
+lift_motor = Motor(Port.C)
 
 
+# --- TEST FIELD ---
+#turning.turn_to(80, horizontal_gyro, left_drive_motor, right_drive_motor, lift_motor)
+#turning.turn_to(-80, horizontal_gyro, left_drive_motor, right_drive_motor, lift_motor)
+# ------------------
 
-DRIVE_SPEED = 360 * 2
 
 ev3.screen.draw_text(10, 10, "waiting to start")
 
-triggering_angle = 10
+left_drive_motor.reset_angle(0)
+right_drive_motor.reset_angle(0)
+triggering_angle = 5
 while left_drive_motor.angle() < triggering_angle and right_drive_motor.angle() < triggering_angle:
     pass
 
@@ -44,14 +51,19 @@ ev3.screen.clear()
 
 while True:
 
-    if kill_switch.pressed():
-        quit()
-
     if program_state == ORIENTING_STATE:
-        orientation.orientationStep()
-        program_state = CLIMBING_STATE
+        orientation_result = orientation.orientationStep(
+            ev3, left_drive_motor, right_drive_motor, 
+            lift_motor, distance_sensor, horizontal_gyro)
+        if orientation_result is True:
+            program_state = CLIMBING_STATE
+        else:
+            ev3.screen.clear()
+            ev3.screen.draw_text(10, 10, "ERROR WHILE ORIENTING")
+
     
     if program_state == CLIMBING_STATE:
-        left_drive_motor.run(DRIVE_SPEED)
-        right_drive_motor.run(DRIVE_SPEED)
+        #left_drive_motor.run(DRIVE_SPEED)
+        #right_drive_motor.run(DRIVE_SPEED)
+        pass
             
--- orientation.py
+++ orientation.py
@@ -2,15 +2,14 @@ from pybricks.hubs import EV3Brick
 from pybricks.ev3devices import Motor, UltrasonicSensor, GyroSensor
 from pybricks.parameters import Stop
 
-LEVEL = [125, 135]
-VERY_LOW = [100, 125]
-LOW = [80, 100]
-IDEAL = [60, 80]
-HIGH = [40, 80]
-VERY_HIGH = [0, 40]
+import turning
 
-CRAWLING_SPEED = 180
-TURNING_SPEED = 180
+LEVEL = [110, 135]
+LOW = [100, 110]
+IDEAL = [70, 100]
+HIGH = [0, 70]
+
+CRAWLING_SPEED = 360
 BACKING_UP_TIME = 2
 
 LEFT = "LEFT"
@@ -21,8 +20,9 @@ def orientationStep(
         ev3: EV3Brick,
         left_motor: Motor, 
         right_motor: Motor, 
+        lift_motor: Motor,
         ultrasonic: UltrasonicSensor, 
-        horizontal_gyro: GyroSensor):
+        horizontal_gyro: GyroSensor) -> bool:
     # slowly move forward
     # if the distance changes: (check for misreadings)
     #   1) save it (not ideal or a step down)
@@ -45,19 +45,26 @@ def orientationStep(
         left_motor.run(CRAWLING_SPEED)
         right_motor.run(CRAWLING_SPEED)
 
+        counter = 0
         while LEVEL[0] < ultrasonic.distance() < LEVEL[1]:
+            counter += 1
+            if counter % 200 == 0:
+                ev3.screen.clear()
+                ev3.screen.draw_text(10, 10, "dist: " + str(ultrasonic.distance()))
             pass
 
         
         left_motor.brake()
         right_motor.brake()
         crawled_distance = (left_motor.angle() + right_motor.angle())/2
+        print("crawled distance ", crawled_distance)
 
         # TODO: check for misreadings, if there are any: move slightly
 
         # output current state to display
         ev3.screen.clear()
-        ev3.screen.draw_text(10, 10, "found step with height " + ultrasonic.distance())
+        ev3.screen.draw_text(10, 10, "found step")
+        ev3.screen.draw_text(10, 30,  "with height " + str(ultrasonic.distance()))
 
         distance_reading = ultrasonic.distance()
 
@@ -69,22 +76,24 @@ def orientationStep(
             left_step_distance = distance_reading
 
         if isIdealStep(distance_reading):
-            break
+            ev3.screen.clear()
+            ev3.screen.draw_text(10, 10, "going for " + currently_exploring_angle)
+            return True
         else:
             # back up and turn 90deg
-            left_motor.run_angle(-CRAWLING_SPEED, crawled_distance, Stop.BRAKE)
-            right_motor.run_angle(-CRAWLING_SPEED, crawled_distance, Stop.BRAKE)
-
-            left_motor.run(TURNING_SPEED)
-            right_motor.run(-TURNING_SPEED)
-
-            while horizontal_gyro.angle() < 90:
-                pass
-            left_motor.brake()
-            right_motor.brake()
+            left_motor.run_angle(-CRAWLING_SPEED, crawled_distance*0.5, Stop.BRAKE, wait=False)
+            right_motor.run_angle(-CRAWLING_SPEED, crawled_distance*0.5, Stop.BRAKE, wait=True)
 
             if currently_exploring_angle is CENTER:
+                turning.turn_to(80, horizontal_gyro, left_motor, right_motor, lift_motor)
                 currently_exploring_angle = RIGHT
+            elif currently_exploring_angle is RIGHT:
+                turning.turn_to(-180, horizontal_gyro, left_motor, right_motor, lift_motor)
+                currently_exploring_angle = LEFT
+            else:
+                # TODO: find ideal step and take it.
+                return False
+                
             
 
 
